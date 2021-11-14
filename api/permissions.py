bc2a6719f2b1efdacf10e097a3e60d179c0218ed
ilya-goldin <rtrwv88@gmail.com>
2021-11-14 13:40:55 +0300

    feat: add permissions check for shop owner and read only views

--- api/permissions.py
+++ api/permissions.py
@@ -3,5 +3,16 @@ from rest_framework import permissions
 
 class IsPartner(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.type == 'shop':
+        return request.user.type == 'shop'
+
+
+class IsShopOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
             return True
+        return bool(request.user and request.user.is_superuser)
